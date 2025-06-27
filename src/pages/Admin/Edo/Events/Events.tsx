8b89b3a3911e6d1c0bb86c9b47e6a4f010db6fde
Valentin <none>
2025-06-27 10:27:42 +0800

    fix: Временно описал типы данных в компонент

--- src/pages/Admin/Edo/Events/Events.tsx
+++ src/pages/Admin/Edo/Events/Events.tsx
@@ -1,6 +1,5 @@
 import {JSX, useState} from "react";
 import style from "./Events.module.css";
-import {EventType} from "../../../../types";
 import EventItem from "../../../../components/ui/Event/Event.tsx";
 import InputText from "../../../../components/ui/InputText/InputText.tsx";
 import {
@@ -69,7 +68,14 @@ function Events(): JSX.Element {
                 ) : listLoading ? (
                     <>Загрузка...</>
                 ) : listData && listData.length > 0 ? (
-                    listData.map((item: EventType) => {
+                    listData.map((item: {
+                        id: number;
+                        title: string;
+                        description: string;
+                        department: string;
+                        time: string;
+                        date: string;
+                    }) => {
                         return (
                             <EventItem key={item.id} event={item} mutation={deleteEdoEvent}/>
                         )
