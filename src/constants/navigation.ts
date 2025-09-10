58d7da223338cb47256eb35f57967b61c8689118
Valentin <>
2025-09-10 13:37:30 +0800

    fix: Забыл добавить в коммит

---
+++ src/constants/navigation.ts
@@ -0,0 +1,115 @@
+import {ROUTES} from "./routes.ts";
+import homeIcon from '../assets/images/icons/home.svg';
+import personIcon from '../assets/images/icons/person.svg';
+import medalIcon from '../assets/images/icons/medal.svg';
+import libraryIcon from '../assets/images/icons/library.svg';
+import bookIcon from '../assets/images/icons/book.svg';
+import calendarIcon from '../assets/images/icons/calendar.svg';
+import peopleIcon from '../assets/images/icons/people.svg';
+
+export const HOME_NAV_LINKS = [
+    {
+        id: 1,
+        name: 'Главная',
+        path: ROUTES.HOME,
+        icon: homeIcon,
+    },
+    {
+        id: 2,
+        name: 'Карьера',
+        path: ROUTES.CAREER,
+        icon: personIcon,
+    },
+    {
+        id: 3,
+        name: "Достижения",
+        path: "achievements",
+        icon: medalIcon,
+    },
+    {
+        id: 4,
+        name: 'База знаний',
+        path: ROUTES.KNOWLEDGE,
+        icon: libraryIcon,
+    },
+    {
+        id: 5,
+        name: 'Обучение',
+        path: ROUTES.EDUCATION,
+        icon: bookIcon,
+    },
+    {
+        id: 6,
+        name: 'ЕДО',
+        path: ROUTES.EDO,
+        icon: calendarIcon,
+    }
+] as const;
+
+export const ADMIN_NAV_LINKS = [
+    {
+        id: 1,
+        name: 'Пользователи',
+        path: ROUTES.ADMIN_USERS,
+        icon: peopleIcon,
+    },
+    {
+        id: 2,
+        name: 'База знаний',
+        path: ROUTES.ADMIN_KNOWLEDGE,
+        icon: libraryIcon,
+    },
+    {
+        id: 3,
+        name: 'Обучение',
+        path: ROUTES.ADMIN_EDUCATION,
+        icon: bookIcon,
+    },
+    {
+        id: 4,
+        name: 'ЕДО',
+        path: ROUTES.ADMIN_EDO,
+        icon: calendarIcon,
+    }
+] as const;
+
+export const ADMIN_NAV_EDUCATION_LINKS = [
+    {
+        id: 1,
+        title: "Мероприятия",
+        path: ROUTES.ADMIN_EDUCATION_EVENTS,
+    },
+    {
+        id: 2,
+        title: "Электронные курсы",
+        path: ROUTES.ADMIN_EDUCATION_COURSE,
+    },
+    {
+        id: 3,
+        title: "Вебинары",
+        path: ROUTES.ADMIN_EDUCATION_WEBINARS,
+    },
+    {
+        id: 4,
+        title: "Тесты",
+        path: ROUTES.ADMIN_EDUCATION_TESTS,
+    },
+] as const;
+
+export const ADMIN_NAV_EDO_LINKS = [
+    {
+        id: 1,
+        title: "Мероприятия",
+        path: ROUTES.ADMIN_EDO_EVENTS,
+    },
+    {
+        id: 2,
+        title: "Электронные курсы",
+        path: ROUTES.ADMIN_EDO_COURSES,
+    },
+    {
+        id: 3,
+        title: "Тесты",
+        path: ROUTES.ADMIN_EDO_TESTS,
+    },
+] as const;
